4c4efe8a1e49bd6d80721aee7738d5e0b3078c04
fanqie <2917545349@qq.com>
2021-08-13 10:31:17 +0800

    优化sql

--- es-api/src/main/resources/my.sql
+++ es-api/src/main/resources/my.sql
@@ -1,6 +1,37 @@
-select psl.id as id,psl.seal_id as sealId, ps.es_id as esId, ps.name as sealName, pu.id as userId, pu.user_name as userName,pd.id as sealDeptId,pd.dept_name as sealDeptName,(case psl.business_type when '' then 0  when '系统签章' then 1  when '页面签章' then 2 when 'SDK' then 3 else 0 end ) as businessType, date_format(psl.action_time,'%Y-%m-%d %H:%i:%S')  as actionTime,psl.action_time  as actionTimeStamp
-from ps_seal_log psl
-         left join ps_seal ps on psl.seal_id = ps.id
-         left join ps_user pu on psl.user_id = pu.id
-         left join ps_seal_dept psd on psl.seal_id = psd.seal_id
-         left join ps_department pd on psd.dept_id = pd.id where  psl.id > :sql_last_value
+SELECT
+	psl.id AS id,
+	psl.seal_id AS sealId,
+	ps.es_id AS esId,
+	ps. NAME AS sealName,
+	pu.id AS userId,
+	pu.user_name AS userName,
+	pd.id AS sealDeptId,
+	pd.dept_name AS sealDeptName,
+	(
+		CASE psl.business_type
+		WHEN '' THEN
+			0
+		WHEN '系统签章' THEN
+			1
+		WHEN '页面签章' THEN
+			2
+		WHEN 'SDK' THEN
+			3
+		ELSE
+			0
+		END
+	) AS businessType,
+	date_format(
+		psl.action_time,
+		'%Y-%m-%d %H:%i:%S'
+	) AS actionTime,
+	psl.action_time AS actionTimeStamp
+FROM
+	(select id,seal_id,user_id,business_type,action_time from ps_seal_log  where id > :sql_last_value ) psl
+LEFT JOIN ps_seal ps ON psl.seal_id = ps.id
+LEFT JOIN ps_user pu ON psl.user_id = pu.id
+LEFT JOIN ps_seal_dept psd ON psl.seal_id = psd.seal_id
+LEFT JOIN ps_department pd ON psd.dept_id = pd.id;
+
+
+
